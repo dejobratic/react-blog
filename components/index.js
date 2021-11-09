2b08cdfec275e1229b4d3c8141fe9be17253a3b0
Dejan Bratić <dejobratic@gmail.com>
2021-11-09 21:28:25 +0100

    Added initial layout to home page with some components.

---
+++ components/index.js
@@ -0,0 +1,3 @@
+export { default as Categories } from 'components/categories/Categories'
+export { default as PostCard } from 'components/post-card/PostCard'
+export { default as PostWidget } from 'components/post-widget/PostWidget'
--- pages/_app.jsx
+++ pages/_app.jsx
@@ -1,6 +1,7 @@
+import 'styles/globals.scss'
 import 'tailwindcss/tailwind.css'
 
-function MyApp({ Component, pageProps }) {
+const MyApp = ({ Component, pageProps }) => {
   return <Component {...pageProps} />
 }
 
