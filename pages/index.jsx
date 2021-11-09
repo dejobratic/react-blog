43dc6f06a3de501fb54541eaef73c5966b8ce14c
Dejan Bratić <dejobratic@gmail.com>
2021-11-09 21:02:14 +0100

    Initial commit

---
+++ pages/index.jsx
@@ -0,0 +1,14 @@
+import Head from 'next/head'
+
+const Home = () => {
+  return (
+    <div className='container mx-auto px-10 mb-8'>
+      <Head>
+        <title>React Blog</title>
+        <link rel='icon' href='/favicon.ico' />
+      </Head>
+    </div>
+  )
+}
+
+export default Home
